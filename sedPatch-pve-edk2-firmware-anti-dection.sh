ee91b4472e9196af470e78283a14db1b5d3e026c
李晓流 <2900629805@qq.com>
2025-04-01 22:48:15 +0800

    Update sedPatch-pve-edk2-firmware-anti-dection.sh

--- sedPatch-pve-edk2-firmware-anti-dection.sh
+++ sedPatch-pve-edk2-firmware-anti-dection.sh
@@ -5,11 +5,11 @@ echo "开始sed工作"
 sed -i 's/0x20202020324B4445/0x20202020204c5450/g' MdeModulePkg/MdeModulePkg.dec
 sed -i 's/QemuFwCfgAcpiPlatform/AsusFwCfgAcpiPlatform/g' OvmfPkg/AcpiPlatformDxe/AcpiPlatformDxe.inf
 sed -i 's/= VirtioFdtDxe/= AsusioFdtDxe/g' OvmfPkg/Fdt/VirtioFdtDxe/VirtioFdtDxe.inf
-sed -i 's/= QemuKernelLoaderFsDxe/= AsusKernelLoaderFsDxe/g' OvmfPkg/QemuKernelLoaderFsDxe/QemuKernelLoaderFsDxe.inf
+sed -i 's/BASE_NAME                      = QemuKernelLoaderFsDxe/BASE_NAME                      = AsusKernelLoaderFsDxe/g' OvmfPkg/QemuKernelLoaderFsDxe/QemuKernelLoaderFsDxe.inf
 sed -i 's/= QemuRamfbDxe/= AsusRamfbDxe/g' OvmfPkg/QemuRamfbDxe/QemuRamfbDxe.inf
 sed -i 's/= QemuVideoDxe/= AsusVideoDxe/g' OvmfPkg/QemuVideoDxe/QemuVideoDxe.inf
 sed -i 's/= VirtHstiDxe/= AsusHstiDxe/g' OvmfPkg/VirtHstiDxe/VirtHstiDxe.inf
-sed -i 's/= Virtio10/= Asusio10/g' OvmfPkg/Virtio10Dxe/Virtio10.inf
+sed -i 's/BASE_NAME                      = Virtio10/BASE_NAME                      = Asusio10/g' OvmfPkg/Virtio10Dxe/Virtio10.inf
 sed -i 's/= VirtioBlkDxe/= AsusioBlkDxe/g' OvmfPkg/VirtioBlkDxe/VirtioBlk.inf
 sed -i 's/= VirtioFsDxe/= AsusioFsDxe/g' OvmfPkg/VirtioFsDxe/VirtioFsDxe.inf
 sed -i 's/= VirtioGpuDxe/= AsusioGpuDxe/g' OvmfPkg/VirtioGpuDxe/VirtioGpu.inf
@@ -21,10 +21,10 @@ sed -i 's/= VirtioScsiDxe/= AsusioScsiDxe/g' OvmfPkg/VirtioScsiDxe/VirtioScsi.in
 sed -i 's/= VirtioSerialDxe/= AsusioSerialDxe/g' OvmfPkg/VirtioSerialDxe/VirtioSerial.inf
 sed -i 's/= VirtNorFlashDxe/= AsusNorFlashDxe/g' OvmfPkg/VirtNorFlashDxe/VirtNorFlashDxe.inf
 sed -i 's/= XenAcpiPlatform/= AsusAcpiPlatform/g' OvmfPkg/XenAcpiPlatformDxe/XenAcpiPlatformDxe.inf
-sed -i 's/= XenBusDxe/= AsusBusDxe/g' OvmfPkg/XenBusDxe/XenBusDxe.inf
+sed -i 's/BASE_NAME                 = XenBusDxe/BASE_NAME                 = AsusBusDxe/g' OvmfPkg/XenBusDxe/XenBusDxe.inf
 sed -i 's/= XenIoPciDxe/= AsusIoPciDxe/g' OvmfPkg/XenIoPciDxe/XenIoPciDxe.inf
 sed -i 's/= XenIoPvhDxe/= AsusIoPvhDxe/g' OvmfPkg/XenIoPvhDxe/XenIoPvhDxe.inf
 sed -i 's/= XenPlatformPei/= AsusPlatformPei/g' OvmfPkg/XenPlatformPei/XenPlatformPei.inf
-sed -i 's/= XenPvBlkDxe/= AsusPvBlkDxe/g' OvmfPkg/XenPvBlkDxe/XenPvBlkDxe.inf
+sed -i 's/BASE_NAME                 = XenPvBlkDxe/BASE_NAME                 = AsusPvBlkDxe/g' OvmfPkg/XenPvBlkDxe/XenPvBlkDxe.inf
 sed -i 's/= XenResetVector/= AsusResetVector/g' OvmfPkg/XenResetVector/XenResetVector.inf
 echo "结束sed工作"
