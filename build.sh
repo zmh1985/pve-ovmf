4a58a969662c71e2c6666684dc0dfccb671c4cf8
李晓流 <2900629805@qq.com>
2025-04-01 21:46:35 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#以下两行docker里面运行需要sudo 如果pve里面运行请删除sudo
 sudo apt-get update
 sudo apt-get install -y pve-qemu-kvm gcc-aarch64-linux-gnu gcc-riscv64-linux-gnu libacl1-dev libaio-dev libattr1-dev libcap-ng-dev libcurl4-gnutls-dev libepoxy-dev libfdt-dev libgbm-dev libglusterfs-dev libgnutls28-dev libiscsi-dev libjpeg-dev libnuma-dev libpci-dev libpixman-1-dev libproxmox-backup-qemu0-dev librbd-dev libsdl1.2-dev libseccomp-dev libslirp-dev libspice-protocol-dev libspice-server-dev libsystemd-dev liburing-dev libusb-1.0-0-dev libusbredirparser-dev libvirglrenderer-dev meson python3-sphinx python3-sphinx-rtd-theme quilt xfslibs-dev
 ls
@@ -7,8 +8,7 @@ cd pve-edk2-firmware
 #git reset --hard 17443032f78eaf9ae276f8df9d10c64beec2e048
 apt install devscripts -y
 mk-build-deps --install
-make
-make clean
+git submodule update --init --recursive
 cp ../Logo.bmp debian/
 cp ../sedPatch-pve-edk2-firmware-anti-dection.sh edk2/
 cd edk2
