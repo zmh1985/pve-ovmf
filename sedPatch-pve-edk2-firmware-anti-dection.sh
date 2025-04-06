faea572db36a99e02303572e47e471d98545019b
李晓流 <2900629805@qq.com>
2025-04-06 21:33:35 +0800

    Update sedPatch-pve-edk2-firmware-anti-dection.sh

--- sedPatch-pve-edk2-firmware-anti-dection.sh
+++ sedPatch-pve-edk2-firmware-anti-dection.sh
@@ -28,4 +28,26 @@ sed -i 's/= XenIoPvhDxe/= '${brand}'IoPvhDxe/g' OvmfPkg/XenIoPvhDxe/XenIoPvhDxe.
 sed -i 's/= XenPlatformPei/= '${brand}'PlatformPei/g' OvmfPkg/XenPlatformPei/XenPlatformPei.inf
 sed -i 's/BASE_NAME                 = XenPvBlkDxe/BASE_NAME                 = '${brand}'PvBlkDxe/g' OvmfPkg/XenPvBlkDxe/XenPvBlkDxe.inf
 sed -i 's/= XenResetVector/= '${brand}'ResetVector/g' OvmfPkg/XenResetVector/XenResetVector.inf
+sed -i 's/BASE_NAME                      = XenSmbiosPlatformDxe/BASE_NAME                      = '${brand}'SmbiosPlatformDxe/g' OvmfPkg/SmbiosPlatformDxe/XenSmbiosPlatformDxe.inf
+sed -i 's/BASE_NAME                      = XenIoMmioLib/BASE_NAME                      = '${brand}'IoMmioLib/g' OvmfPkg/Library/XenIoMmioLib/XenIoMmioLib.inf
+sed -i 's/BASE_NAME                      = XenPlatformLib/BASE_NAME                      = '${brand}'PlatformLib/g' OvmfPkg/Library/XenPlatformLib/XenPlatformLib.inf
+sed -i 's/BASE_NAME                      = QemuFwCfgDxeLib/BASE_NAME                      = '${brand}'FwCfgDxeLib/g' OvmfPkg/Library/QemuFwCfgLib/QemuFwCfgDxeLib.inf
+sed -i 's/BASE_NAME                      = QemuFwCfgLibNull/BASE_NAME                      = '${brand}'FwCfgLibNull/g' OvmfPkg/Library/QemuFwCfgLib/QemuFwCfgLibNull.inf
+sed -i 's/BASE_NAME                      = QemuFwCfgLib/BASE_NAME                      = '${brand}'FwCfgLib/g' OvmfPkg/Library/QemuFwCfgLib/QemuFwCfgMmioDxeLib.inf
+sed -i 's/BASE_NAME                      = QemuFwCfgPeiLib/BASE_NAME                      = '${brand}'FwCfgPeiLib/g' OvmfPkg/Library/QemuFwCfgLib/QemuFwCfgMmioPeiLib.inf
+sed -i 's/BASE_NAME                      = QemuFwCfgPeiLib/BASE_NAME                      = '${brand}'FwCfgPeiLib/g' OvmfPkg/Library/QemuFwCfgLib/QemuFwCfgPeiLib.inf
+sed -i 's/BASE_NAME                      = QemuFwCfgSecLib/BASE_NAME                      = '${brand}'FwCfgSecLib/g' OvmfPkg/Library/QemuFwCfgLib/QemuFwCfgSecLib.inf
+sed -i 's/BASE_NAME                      = XenHypercallLib/BASE_NAME                      = '${brand}'HypercallLib/g' OvmfPkg/Library/XenHypercallLib/XenHypercallLib.inf
+sed -i 's/BASE_NAME                      = QemuBootOrderLib/BASE_NAME                      = '${brand}'BootOrderLib/g' OvmfPkg/Library/QemuBootOrderLib/QemuBootOrderLib.inf
+sed -i 's/BASE_NAME                      = VirtioMmioDeviceLib/BASE_NAME                      = '${brand}'ioMmioDeviceLib/g' OvmfPkg/Library/VirtioMmioDeviceLib/VirtioMmioDeviceLib.inf
+sed -i 's/BASE_NAME                      = X86QemuLoadImageLib/BASE_NAME                      = X86'${brand}'LoadImageLib/g' OvmfPkg/Library/X86QemuLoadImageLib/X86QemuLoadImageLib.inf
+sed -i 's/BASE_NAME                      = XenRealTimeClockLib/BASE_NAME                      = '${brand}'RealTimeClockLib/g' OvmfPkg/Library/XenRealTimeClockLib/XenRealTimeClockLib.inf
+sed -i 's/BASE_NAME                      = XenConsoleSerialPortLib/BASE_NAME                      = '${brand}'ConsoleSerialPortLib/g' OvmfPkg/Library/XenConsoleSerialPortLib/XenConsoleSerialPortLib.inf
+sed -i 's/BASE_NAME                      = QemuFwCfgSimpleParserLib/BASE_NAME                      = '${brand}'FwCfgSimpleParserLib/g' OvmfPkg/Library/QemuFwCfgSimpleParserLib/QemuFwCfgSimpleParserLib.inf
+sed -i 's/BASE_NAME                      = VirtNorFlashStaticLib/BASE_NAME                      = '${brand}'NorFlashStaticLib/g' OvmfPkg/Library/VirtNorFlashPlatformLib/VirtNorFlashStaticLib.inf
+sed -i 's/BASE_NAME                      = VirtNorFlashDeviceTreeLib/BASE_NAME                      = '${brand}'NorFlashDeviceTreeLib/g' OvmfPkg/Library/VirtNorFlashPlatformLib/VirtNorFlashDeviceTreeLib.inf
+sed -i 's/BASE_NAME                      = BaseQemuFwCfgS3LibNull/BASE_NAME                      = Base'${brand}'FwCfgS3LibNull/g' OvmfPkg/Library/QemuFwCfgS3Lib/BaseQemuFwCfgS3LibNull.inf
+sed -i 's/BASE_NAME                      = DxeQemuFwCfgS3LibFwCfg/BASE_NAME                      = Dxe'${brand}'FwCfgS3LibFwCfg/g' OvmfPkg/Library/QemuFwCfgS3Lib/DxeQemuFwCfgS3LibFwCfg.inf
+sed -i 's/BASE_NAME                      = PeiQemuFwCfgS3LibFwCfg/BASE_NAME                      = Pei'${brand}'FwCfgS3LibFwCfg/g' OvmfPkg/Library/QemuFwCfgS3Lib/PeiQemuFwCfgS3LibFwCfg.inf
+sed -i 's/BASE_NAME                      = GenericQemuLoadImageLib/BASE_NAME                      = Generic'${brand}'LoadImageLib/g' OvmfPkg/Library/GenericQemuLoadImageLib/GenericQemuLoadImageLib.inf
 echo "结束sed工作"
